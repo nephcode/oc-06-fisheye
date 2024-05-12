604b722b0db8bd90a6db440131b23f86f2ad858f
Néphélim <nephcohen@gmail.com>
2024-05-12 18:07:32 +0200

    Design Pattern progress 01
    update

--- js/factories/display.js
+++ js/factories/display.js
@@ -1,25 +1,60 @@
 //======= ∵ ƸӜƷ ∴ ============================ NEPHA =========//
 //======================= ∵ NPƸӜƷL1M ∴ =======================//
 //================================================ 2024 ======//
-class ArtistFetcher{
-    /// code 
 
-    constructor(){
-        this.artist = [];
-    }
+class FactoryArtist{
+    constructor(artistId, artistName){
+        this._artistId = artistId;
+        this._artistName = artistName;
+      }
+}
+
+class ArtistProfil extends FactoryArtist{
+    constructor()
 
 }
 
 class FactoryMedia{
-    //code
+    constructor(data){
+        this.id = data.id;
+        this.photographerId = data.photographerId;
+        this.title = data.title;
+        this.likes = data.likes;
+        this.date = new Date(data.date);
+        this.price = data.price;
+    }
+
+	render() {
+    	// Méthode générique pour rendre un média; à spécialiser dans les sous-classes
+        console.log('Render not implemented for base Media class.');
+    }
 }
 
 class MediaImage extends FactoryMedia {
-    //code 
+    constructor(data) {
+        super(data);
+        this.image = data.image;
+    }
+
+	render() {
+        // Rendu spécifique pour les images
+        return `<img src="/path/to/images/${this.image}" alt="${this.title}" title="${this.title}" />`;
+    }
 }
 
 class MediaVideo extends factoryMedia{
-    //code
+        constructor(data) {
+        super(data);
+        this.video = data.video;
+    }
+
+    render() {
+        // Rendu spécifique pour les vidéos
+        return `<video controls>
+                    <source src="/path/to/videos/${this.video}" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>`;
+    }
 }
 
 //======= ∵ ƸӜƷ ∴ ============================================//
