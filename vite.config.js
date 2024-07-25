036b5a6799ea042213933d697be2b6044a10f93b
Néphélim <nephcohen@gmail.com>
2024-07-25 17:18:05 +0200

    Maj de déploiement
    # Changelog
    
    ### Fixed
    - On remet le **robots.txt** à sa place dans le bon répertoire maggle
    - Suppression des élements readme de la publication
    - Correction du déploiement (et on espère que ça fonctionne)
    
    ### Changed
    - Toujours une météo bizarre et j'ai pas de QR code pour circuler dans Paris pendant les J.O

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   esbuild: {
-    target: 'esnext' // Changez ceci pour un environnement supportant top-level await
-  }
+    target: 'esnext' // Change this to an environment that supports top-level await
+  },
+  server: {
+    host: 'oc06-fisheye.local',
+    port: 5006,
+    https: false,
+  },
+  preview: {
+    port: 5106,
+  },
 });
