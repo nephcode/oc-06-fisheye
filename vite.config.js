8de2f6e8ea7299b3a1c594e2ce69275e3d0e08d1
Néphélim <nephcohen@gmail.com>
2024-08-05 23:50:16 +0200

    on tente une maj

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   esbuild: {
@@ -13,5 +18,16 @@ export default defineConfig({
   preview: {
     host: 'oc06-fisheye.local',
     port: 5106,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        photographer: resolve(__dirname, 'public/photographer.html'),
+        // Ajoutez ici vos fichiers JS que vous voulez compiler
+        script1: resolve(__dirname, 'js/pages/photographer.js'),
+        // Ajoutez autant d'entrées que nécessaire
+      },
+    },
   },
 });
