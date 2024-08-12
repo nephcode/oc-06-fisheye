37d27b4d3f85dbdaf014dd791a7cb103b36bd5b0
Néphélim <nephcohen@gmail.com>
2024-08-12 06:54:32 +0200

    deploy udpate 2
    debug vite & bun update deploy

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Configurations générales pour le développement
-  server: {
-    port: 3000,  // Port sur lequel le serveur de développement va tourner
-    open: true,  // Ouvrir automatiquement le navigateur
-  },
-
-  // Configurations pour la construction
+  root: 'src', // Définit le répertoire racine pour Vite
   build: {
-    outDir: 'dist',  // Répertoire de sortie pour les fichiers construits
-    sourcemap: true, // Générer une source map pour le débogage
-    rollupOptions: {
-      input: 'index.html',  // Point d'entrée principal
-    },
-  },
-
-  // Résolution de modules
-  resolve: {
-    alias: {
-      '@': '/src',  // Exemple d'alias pour accéder plus facilement aux fichiers sources
-    },
+    outDir: '../dist', // Spécifie le répertoire de sortie
   },
 });
